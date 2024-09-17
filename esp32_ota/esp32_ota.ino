50721e06f0bdc7470de7cde5e27387c76cbf732d
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2024-09-17 12:44:53 +0600

    added sketch

---
+++ esp32_ota/esp32_ota.ino
@@ -0,0 +1,46 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <HTTPUpdate.h>
+
+const char* ssid = "BJIT_ADMIN";       // Your Wi-Fi SSID
+const char* password = "Bjit#@dmin";   // Your Wi-Fi Password
+
+const char* firmwareURL = "http://your-server.com/firmware.bin"; // URL of the firmware file
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("Connected to WiFi.");
+
+  // Start the OTA update process
+  t_httpUpdate_return result = ESPhttpUpdate.update(firmwareURL);
+
+  // Check the result of the update process
+  switch (result) {
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("Update failed. Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+      break;
+    
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("No updates available.");
+      break;
+
+    case HTTP_UPDATE_OK:
+      Serial.println("Update successful! Restarting...");
+      ESP.restart();
+      break;
+  }
+}
+
+void loop() {
+  // Nothing to do in the loop
+}
+
